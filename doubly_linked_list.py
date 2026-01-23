cf8820d8d5d621d0ac05f3c090fda87d35c19ef4
Akash Singh <akashsr.contact@gmail.com>
2026-01-23 10:49:35 +0530

    added get method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -71,3 +71,13 @@ class DoublyLinkedList:
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        else:
+            temp = self.tail
+            for _ in range (self.length -1, index, -1):
+                temp = temp.prev
+            return temp
